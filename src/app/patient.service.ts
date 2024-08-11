29b8f1c323b2a132e9586d05244273bed939571f
Nilo Chaves <nilo_1903@hotmail.com>
2024-08-11 20:09:11 -0300

    Altera url localhost para url do deploy no Rendler

--- src/app/patient.service.ts
+++ src/app/patient.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PatientService {
-  private apiUrl = 'http://localhost:8080/pacientes';
+  private apiUrl = 'https://clinica-youx-backend.onrender.com/pacientes';
 
   constructor(private http: HttpClient) { }
 
